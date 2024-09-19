3565de5732bc050d3f8221bc5e98339e14f32baf
DzirtH <68776022+DzirtH@users.noreply.github.com>
2024-09-19 12:29:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ pygame.init()
 WIDTH, HEIGHT = 800, 600
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 
-pygame.display.set_caption("Gradational Slightshot Effect")
+pygame.display.set_caption("Jupiter")
 
 PLANET_MASS = 100
 SHIP_MASS = 5
